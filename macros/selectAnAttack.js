2eb986ad4a3b501bc77d531b57daa3e94ca71221
Calvin <calvin.s.mcgowan@gmail.com>
2020-12-31 01:50:56 -0700

    Proof of concept. Select a target, and the macro sets their HP to 9.

--- macros/selectAnAttack.js
+++ macros/selectAnAttack.js
@@ -1,4 +1,58 @@
-let self = token.actor.data.data;
-let target = game.user.targets.keys().next().value.actor.data.data;
-console.log(self);
-console.log(target);
+let selfToken = token;//Get owned token
+let targetSet = game.user.targets//Get set of targets
+let targetArray = Array.from(targetSet);//Convert to an actually useable data type
+let selfActor = selfToken.actor.data.data//Get owned actor from token
+
+console.log(selfToken);
+console.log(selfActor);
+console.log(targetArray);
+
+
+//This is the bit where we figure out where every one is, along with distances and range penalties.
+let selfCoords = selfToken._validPosition
+
+let targetCoords = "";
+
+
+
+//Assemble modal content
+let htmlContent = "";
+htmlContent += "<select id='target' name='target'>"
+
+for (let a = 0; a < targetArray.length; a++){
+    htmlContent += "<option value='" + a + "'>" + targetArray[a].nameplate._text + "</option>"
+}
+
+htmlContent += "</select>"
+//
+
+let selectionModal = new Dialog({
+    title: "SHOW ME YOUR MOVES",
+    content: htmlContent,
+    buttons: {
+        mod: {
+            icon: '<i class="fas fa-check"></i>',
+            label: "Attack",
+            callback: (html) => {
+                console.log(html.find('#target').val())
+                console.log(game.actors.get(targetArray[html.find('#target').val()].data.actorId))
+                game.actors.get(targetArray[html.find('#target').val()].data.actorId).data.data.reserves.hp.value = 9
+                console.log(game.actors.get(targetArray[html.find('#target').val()].data.actorId).data.data.reserves.hp.value)
+            }
+        },
+        noMod: {
+            icon: '<i class="fas fa-times"></i>',
+            label: "Cancel",
+            callback: () => {
+
+            }
+        }
+    },
+    default: "noMod",
+    render: html => console.log("Register interactivity in the rendered dialog"),
+    close: html => console.log("This always is logged no matter which option is chosen")
+})
+selectionModal.render(true)
+
+
+console.log(selfCoords)
