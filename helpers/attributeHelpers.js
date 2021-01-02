4c33d37438f4ab487bae208b52a13aacb212b45f
Calvin <calvin.s.mcgowan@gmail.com>
2021-01-01 17:27:42 -0700

    Attribute helpers first commit with swing and thrust calculating methods.

---
+++ helpers/attributeHelpers.js
@@ -0,0 +1,625 @@
+export const strikingStrengthToThrust = (sst) => {
+
+    let thrust = "";
+
+    switch(sst) {
+        case 1:
+            thrust = "1d6-6";
+            break;
+        case 2:
+            thrust = "1d6-6";
+            break;
+        case 3:
+            thrust = "1d6-5";
+            break;
+        case 4:
+            thrust = "1d6-5";
+            break;
+        case 5:
+            thrust = "1d6-4";
+            break;
+        case 6:
+            thrust = "1d6-4";
+            break;
+        case 7:
+            thrust = "1d6-3";
+            break;
+        case 8:
+            thrust = "1d6-3";
+            break;
+        case 9:
+            thrust = "1d6-2";
+            break;
+        case 10:
+            thrust = "1d6-2";
+            break;
+        case 11:
+            thrust = "1d6-1";
+            break;
+        case 12:
+            thrust = "1d6-1";
+            break;
+        case 13:
+            thrust = "1d6";
+            break;
+        case 14:
+            thrust = "1d6";
+            break;
+        case 15:
+            thrust = "1d6+1";
+            break;
+        case 16:
+            thrust = "1d6+1";
+            break;
+        case 17:
+            thrust = "1d6+2";
+            break;
+        case 18:
+            thrust = "1d6+2";
+            break;
+        case 19:
+            thrust = "2d6-1";
+            break;
+        case 20:
+            thrust = "2d6-1";
+            break;
+        case 21:
+            thrust = "2d6";
+            break;
+        case 22:
+            thrust = "2d6";
+            break;
+        case 23:
+            thrust = "2d6+1";
+            break;
+        case 24:
+            thrust = "2d6+1";
+            break;
+        case 25:
+            thrust = "2d6+2";
+            break;
+        case 26:
+            thrust = "2d6+2";
+            break;
+        case 27:
+            thrust = "3d6-1";
+            break;
+        case 28:
+            thrust = "3d6-1";
+            break;
+        case 29:
+            thrust = "3d6";
+            break;
+        case 30:
+            thrust = "3d6";
+            break;
+        case 31:
+            thrust = "3d6-+1";
+            break;
+        case 32:
+            thrust = "3d6+1";
+            break;
+        case 33:
+            thrust = "3d6+2";
+            break;
+        case 34:
+            thrust = "3d6+2";
+            break;
+        case 35:
+            thrust = "4d6-1";
+            break;
+        case 36:
+            thrust = "4d6-1";
+            break;
+        case 37:
+            thrust = "4d6";
+            break;
+        case 38:
+            thrust = "4d6";
+            break;
+        case 39:
+            thrust = "4d6+1";
+            break;
+        case 40:
+            thrust = "4d6+1";
+            break;
+        case 41:
+            thrust = "4d6+1";
+            break;
+        case 42:
+            thrust = "4d6+1";
+            break;
+        case 43:
+            thrust = "4d6+1";
+            break;
+        case 44:
+            thrust = "4d6+1";
+            break;
+        case 45:
+            thrust = "5d6";
+            break;
+        case 46:
+            thrust = "5d6";
+            break;
+        case 47:
+            thrust = "5d6";
+            break;
+        case 48:
+            thrust = "5d6";
+            break;
+        case 49:
+            thrust = "5d6";
+            break;
+        case 50:
+            thrust = "5d6+2";
+            break;
+        case 51:
+            thrust = "5d6+2";
+            break;
+        case 52:
+            thrust = "5d6+2";
+            break;
+        case 53:
+            thrust = "5d6+2";
+            break;
+        case 54:
+            thrust = "5d6+2";
+            break;
+        case 55:
+            thrust = "6d6";
+            break;
+        case 56:
+            thrust = "6d6";
+            break;
+        case 57:
+            thrust = "6d6";
+            break;
+        case 58:
+            thrust = "6d6";
+            break;
+        case 59:
+            thrust = "6d6";
+            break;
+        case 60:
+            thrust = "7d6-1";
+            break;
+        case 61:
+            thrust = "7d6-1";
+            break;
+        case 62:
+            thrust = "7d6-1";
+            break;
+        case 63:
+            thrust = "7d6-1";
+            break;
+        case 64:
+            thrust = "7d6-1";
+            break;
+        case 65:
+            thrust = "7d6+1";
+            break;
+        case 66:
+            thrust = "7d6+1";
+            break;
+        case 67:
+            thrust = "7d6+1";
+            break;
+        case 68:
+            thrust = "7d6+1";
+            break;
+        case 69:
+            thrust = "7d6+1";
+            break;
+        case 70:
+            thrust = "8d6";
+            break;
+        case 71:
+            thrust = "8d6";
+            break;
+        case 72:
+            thrust = "8d6";
+            break;
+        case 73:
+            thrust = "8d6";
+            break;
+        case 74:
+            thrust = "8d6";
+            break;
+        case 75:
+            thrust = "8d6+2";
+            break;
+        case 76:
+            thrust = "8d6+2";
+            break;
+        case 77:
+            thrust = "8d6+2";
+            break;
+        case 78:
+            thrust = "8d6+2";
+            break;
+        case 79:
+            thrust = "8d6+2";
+            break;
+        case 80:
+            thrust = "9d6";
+            break;
+        case 81:
+            thrust = "9d6";
+            break;
+        case 82:
+            thrust = "9d6";
+            break;
+        case 83:
+            thrust = "9d6";
+            break;
+        case 84:
+            thrust = "9d6";
+            break;
+        case 85:
+            thrust = "9d6+2";
+            break;
+        case 86:
+            thrust = "9d6+2";
+            break;
+        case 87:
+            thrust = "9d6+2";
+            break;
+        case 88:
+            thrust = "9d6+2";
+            break;
+        case 89:
+            thrust = "9d6+2";
+            break;
+        case 90:
+            thrust = "10d6";
+            break;
+        case 91:
+            thrust = "10d6";
+            break;
+        case 92:
+            thrust = "10d6";
+            break;
+        case 93:
+            thrust = "10d6";
+            break;
+        case 94:
+            thrust = "10d6";
+            break;
+        case 95:
+            thrust = "10d6+2";
+            break;
+        case 96:
+            thrust = "10d6+2";
+            break;
+        case 97:
+            thrust = "10d6+2";
+            break;
+        case 98:
+            thrust = "10d6+2";
+            break;
+        case 99:
+            thrust = "10d6+2";
+            break;
+        case 100:
+            thrust = "11d6";
+            break;
+        default:
+            thrust = "11d6";
+            break;
+    }
+
+    return thrust;
+}
+
+export const strikingStrengthToSwing = (sst) => {
+
+    let swing = "";
+
+    switch(sst) {
+        case 1:
+            swing = "1d6-5";
+            break;
+        case 2:
+            swing = "1d6-5";
+            break;
+        case 3:
+            swing = "1d6-4";
+            break;
+        case 4:
+            swing = "1d6-4";
+            break;
+        case 5:
+            swing = "1d6-3";
+            break;
+        case 6:
+            swing = "1d6-3";
+            break;
+        case 7:
+            swing = "1d6-2";
+            break;
+        case 8:
+            swing = "1d6-2";
+            break;
+        case 9:
+            swing = "1d6-1";
+            break;
+        case 10:
+            swing = "1d6";
+            break;
+        case 11:
+            swing = "1d6+1";
+            break;
+        case 12:
+            swing = "1d6+2";
+            break;
+        case 13:
+            swing = "2d6-1";
+            break;
+        case 14:
+            swing = "2d6";
+            break;
+        case 15:
+            swing = "2d6+1";
+            break;
+        case 16:
+            swing = "2d6+2";
+            break;
+        case 17:
+            swing = "3d6-1";
+            break;
+        case 18:
+            swing = "3d6";
+            break;
+        case 19:
+            swing = "3d6+1";
+            break;
+        case 20:
+            swing = "3d6+2";
+            break;
+        case 21:
+            swing = "4d6-1";
+            break;
+        case 22:
+            swing = "4d6";
+            break;
+        case 23:
+            swing = "4d6+1";
+            break;
+        case 24:
+            swing = "4d6+2";
+            break;
+        case 25:
+            swing = "5d6-1";
+            break;
+        case 26:
+            swing = "5d6";
+            break;
+        case 27:
+            swing = "5d6+1";
+            break;
+        case 28:
+            swing = "5d6+1";
+            break;
+        case 29:
+            swing = "5d6+2";
+            break;
+        case 30:
+            swing = "5d6+2";
+            break;
+        case 31:
+            swing = "6d6-1";
+            break;
+        case 32:
+            swing = "6d6-1";
+            break;
+        case 33:
+            swing = "6d6";
+            break;
+        case 34:
+            swing = "6d6";
+            break;
+        case 35:
+            swing = "6d6+1";
+            break;
+        case 36:
+            swing = "6d6+1";
+            break;
+        case 37:
+            swing = "6d6+2";
+            break;
+        case 38:
+            swing = "6d6+2";
+            break;
+        case 39:
+            swing = "7d6-1";
+            break;
+        case 40:
+            swing = "7d6-1";
+            break;
+        case 41:
+            swing = "7d6-1";
+            break;
+        case 42:
+            swing = "7d6-1";
+            break;
+        case 43:
+            swing = "7d6-1";
+            break;
+        case 44:
+            swing = "7d6-1";
+            break;
+        case 45:
+            swing = "7d6+1";
+            break;
+        case 46:
+            swing = "7d6+1";
+            break;
+        case 47:
+            swing = "7d6+1";
+            break;
+        case 48:
+            swing = "7d6+1";
+            break;
+        case 49:
+            swing = "7d6+1";
+            break;
+        case 50:
+            swing = "8d6-1";
+            break;
+        case 51:
+            swing = "8d6-1";
+            break;
+        case 52:
+            swing = "8d6-1";
+            break;
+        case 53:
+            swing = "8d6-1";
+            break;
+        case 54:
+            swing = "8d6-1";
+            break;
+        case 55:
+            swing = "8d6+1";
+            break;
+        case 56:
+            swing = "8d6+1";
+            break;
+        case 57:
+            swing = "8d6+1";
+            break;
+        case 58:
+            swing = "8d6+1";
+            break;
+        case 59:
+            swing = "8d6+1";
+            break;
+        case 60:
+            swing = "9d6";
+            break;
+        case 61:
+            swing = "9d6";
+            break;
+        case 62:
+            swing = "9d6";
+            break;
+        case 63:
+            swing = "9d6";
+            break;
+        case 64:
+            swing = "9d6";
+            break;
+        case 65:
+            swing = "9d6+2";
+            break;
+        case 66:
+            swing = "9d6+2";
+            break;
+        case 67:
+            swing = "9d6+2";
+            break;
+        case 68:
+            swing = "9d6+2";
+            break;
+        case 69:
+            swing = "9d6+2";
+            break;
+        case 70:
+            swing = "10d6";
+            break;
+        case 71:
+            swing = "10d6";
+            break;
+        case 72:
+            swing = "10d6";
+            break;
+        case 73:
+            swing = "10d6";
+            break;
+        case 74:
+            swing = "10d6";
+            break;
+        case 75:
+            swing = "10d6+2";
+            break;
+        case 76:
+            swing = "10d6+2";
+            break;
+        case 77:
+            swing = "10d6+2";
+            break;
+        case 78:
+            swing = "10d6+2";
+            break;
+        case 79:
+            swing = "10d6+2";
+            break;
+        case 80:
+            swing = "11d6";
+            break;
+        case 81:
+            swing = "11d6";
+            break;
+        case 82:
+            swing = "11d6";
+            break;
+        case 83:
+            swing = "11d6";
+            break;
+        case 84:
+            swing = "11d6";
+            break;
+        case 85:
+            swing = "11d6+2";
+            break;
+        case 86:
+            swing = "11d6+2";
+            break;
+        case 87:
+            swing = "11d6+2";
+            break;
+        case 88:
+            swing = "11d6+2";
+            break;
+        case 89:
+            swing = "11d6+2";
+            break;
+        case 90:
+            swing = "12d6";
+            break;
+        case 91:
+            swing = "12d6";
+            break;
+        case 92:
+            swing = "12d6";
+            break;
+        case 93:
+            swing = "12d6";
+            break;
+        case 94:
+            swing = "12d6";
+            break;
+        case 95:
+            swing = "12d6+2";
+            break;
+        case 96:
+            swing = "12d6+2";
+            break;
+        case 97:
+            swing = "12d6+2";
+            break;
+        case 98:
+            swing = "12d6+2";
+            break;
+        case 99:
+            swing = "12d6+2";
+            break;
+        case 100:
+            swing = "13d6";
+            break;
+        default:
+            swing = "13d6";
+            break;
+    }
+
+    return swing;
+}
