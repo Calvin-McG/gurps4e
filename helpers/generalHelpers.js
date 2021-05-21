049a5a941c3c6e33615da2e99e77582965eba79e
Calvin <calvin.s.mcgowan@gmail.com>
2021-05-21 15:23:52 -0600

    Added rof helper

--- helpers/generalHelpers.js
+++ helpers/generalHelpers.js
@@ -7,4 +7,18 @@ export class generalHelpers {
             return true;
         });
     }
+
+    static rofToBonus(rof){
+        let bonus;
+        if      (rof >= 2 && rof <= 4)  { bonus = 0}
+        else if (rof >= 5 && rof <= 8)  { bonus = 1}
+        else if (rof >= 9 && rof <= 12)  { bonus = 2}
+        else if (rof >= 13 && rof <= 16)  { bonus = 3}
+        else if (rof >= 17 && rof <= 24)  { bonus = 4}
+        else if (rof >= 25)  { // At this point the scaling is logarithmic
+            bonus = Math.floor(1.4427 * Math.log(rof) + 0.3562);
+        }
+
+        return bonus;
+    }
 }
