163a55da5b6dbad0f37e8c5595c390587c754a53
Calvin <calvin.s.mcgowan@gmail.com>
2021-05-21 22:05:42 -0600

    Added chat button test

--- module/hooks/renderChatMessage.js
+++ module/hooks/renderChatMessage.js
@@ -5,24 +5,28 @@
 Hooks.on("renderChatMessage", async (app, html, msg) => {
 
   // Hide chat card edit buttons from non-gms
-  if (!game.user.isGM)
-  {
+  if (!game.user.isGM) {
     html.find(".chat-button-gm").remove();
     html.find(".unopposed-button").remove();
     //hide tooltip contextuamneu if not their roll
     if(msg.message.speaker.actor && game.actors.get(msg.message.speaker.actor).permission != 3)
       html.find(".chat-button-player").remove();
   }
-  else
-  {
+  else {
     html.find(".chat-button-player").remove();
   }
 
   // Do not display "Blind" chat cards to non-gm
-  if (html.hasClass("blind") && !game.user.isGM)
-  {
+  if (html.hasClass("blind") && !game.user.isGM) {
     html.find(".message-header").remove(); // Remove header so Foundry does not attempt to update its timestamp
     html.html("").css("display", "none");
   }
 
+  html.find('.test').click(_test.bind(this));
 })
+
+function _test(event) {
+  event.preventDefault();
+  console.log(event);
+  console.log("Test Successful")
+}
