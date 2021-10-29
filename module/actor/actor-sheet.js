6b3f2f96a124ceea9e9400c1690230147c0007bb
Calvin <calvin.s.mcgowan@gmail.com>
2021-10-28 21:15:33 -0600

    Correcting display of character sheet based on new data structure - Removing getData override and unused code in actor-sheet

--- module/actor/actor-sheet.js
+++ module/actor/actor-sheet.js
@@ -20,22 +20,11 @@ export class gurpsActorSheet extends ActorSheet {
 	/* -------------------------------------------- */
 
 	/** @override */
-	getData() {
-		const data = super.getData();
-		data.dtypes = ["String", "Number", "Boolean"];
-
-		return data;
-	}
-
-	/** @override */
 	activateListeners(html) {
 		super.activateListeners(html);
 
 		// Everything below here is only needed if the sheet is editable
 		if (!this.options.editable) return;
-
-		// Add Inventory Item
-		html.find('.item-create').click(this._onItemCreate.bind(this));
 
 		// Update Inventory Item
 		html.find('.item-edit').click(ev => {
@@ -82,33 +71,6 @@ export class gurpsActorSheet extends ActorSheet {
 	_onUnspentPointsChange(event) {
 		let unspent = event.target.value;
 		this.actor.setTotalPoints(unspent);
-	}
-
-	/**
-	 * Handle creating a new Owned Item for the actor using initial data defined in the HTML dataset
-	 * @param {Event} event	 The originating click event
-	 * @private
-	 */
-	_onItemCreate(event) {
-		event.preventDefault();
-		const header = event.currentTarget;
-		// Get the type of item to create.
-		const type = header.dataset.type;
-		// Grab any data associated with this control.
-		const data = duplicate(header.dataset);
-		// Initialize a default name.
-		const name = `New ${type.capitalize()}`;
-		// Prepare the item object.
-		const itemData = {
-			name: name,
-			type: type,
-			data: data
-		};
-		// Remove the type from the dataset since it's in the itemData.type prop.
-		delete itemData.data["type"];
-
-		// Finally, create the item!
-		return this.actor.createOwnedItem(itemData);
 	}
 
 	/**
