e5fb0f1c27fed9fab86ea7dc53b46eb3c637d4f1
Calvin <calvin.s.mcgowan@gmail.com>
2021-11-01 01:40:35 -0600

    Clarifying comment and removing unused setting

--- module/hooks/init.js
+++ module/hooks/init.js
@@ -6,23 +6,13 @@ Hooks.once("init", () => {
   _setGurps4eInitiative();
 
   function _setGurps4eInitiative() {
-    let formula = "@primaryAttributes.speed.value + @primaryAttributes.dexterity.value / 10000 + (1d100 - 1) / 1000000";// First three digits are (speed), then [DX], then {d100-1} (5.00)[10]{38}
+    let formula = "@primaryAttributes.speed.value + @primaryAttributes.dexterity.value / 10000 + (1d100 - 1) / 1000000";// First three digits are (speed), then [DX], then {d100-1}. Example: (5.00)[10]{38} -> 5001038
     let decimals = 6;
     CONFIG.Combat.initiative = {
       formula: formula,
       decimals: decimals
     }
   }
-
-  // Register Hiding Test Data option
-  game.settings.register("gurps4e", "hideTestData", {
-    name: "Hide Test Data",
-    hint: "GM test chat cards don't show sensitive NPC data to players.",
-    scope: "world",
-    config: true,
-    default: true,
-    type: Boolean
-  });
 
   // Register Armour as Dice option
   game.settings.register("gurps4e", "armourAsDice", {
