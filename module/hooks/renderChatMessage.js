45e66c9934f20b030614cd08e3e8a58fa5d9daa1
Calvin <calvin.s.mcgowan@gmail.com>
2021-12-14 18:39:32 -0700

    Removing unused code

--- module/hooks/renderChatMessage.js
+++ module/hooks/renderChatMessage.js
@@ -3,25 +3,6 @@
  */
 
 Hooks.on("renderChatMessage", async (app, html, msg) => {
-
-  // Hide chat card edit buttons from non-gms
-  if (!game.user.isGM) {
-    html.find(".chat-button-gm").remove();
-    html.find(".unopposed-button").remove();
-    //hide tooltip contextuamneu if not their roll
-    if(msg.message.speaker.actor && game.actors.get(msg.message.speaker.actor).permission != 3)
-      html.find(".chat-button-player").remove();
-  }
-  else {
-    html.find(".chat-button-player").remove();
-  }
-
-  // Do not display "Blind" chat cards to non-gm
-  if (html.hasClass("blind") && !game.user.isGM) {
-    html.find(".message-header").remove(); // Remove header so Foundry does not attempt to update its timestamp
-    html.html("").css("display", "none");
-  }
-
   html.find('.attemptActiveDefences').click(attemptActiveDefences.bind(this));
   html.find('.noActiveDefences').click(noActiveDefences.bind(this));
   html.find('.quickContest').click(quickContest.bind(this));
