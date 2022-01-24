1366fcc5ae37989805b6502cacb0885e6883b442
Calvin <calvin.s.mcgowan@gmail.com>
2022-01-24 16:48:31 -0700

    #4 Fixed display and corrected issue where dr and hit location arrows were conflicting.

--- module/item/item-sheet-orig.js
+++ module/item/item-sheet-orig.js
@@ -167,26 +167,26 @@ export class gurpsItemSheet extends ItemSheet {
         if (locationDRBlock.subLocation) { // The location has sub locations
             let locationKeys = Object.keys(locationDRBlock.subLocation);
             for (let d = 0; d < locationKeys.length; d++) {
-                ( (type === "burn" || typeof burn != 'undefined') ? getProperty(locationDRBlock.subLocation, locationKeys[d]).drBurn += +modifier : '');
-                ( (type === "cor" || typeof cor != 'undefined') ? getProperty(locationDRBlock.subLocation, locationKeys[d]).drCor += +modifier : '');
-                ( (type === "cr" || typeof cr != 'undefined') ? getProperty(locationDRBlock.subLocation, locationKeys[d]).drCr += +modifier : '');
-                ( (type === "cut" || typeof cut != 'undefined') ? getProperty(locationDRBlock.subLocation, locationKeys[d]).drCut += +modifier : '');
-                ( (type === "fat" || typeof fat != 'undefined') ? getProperty(locationDRBlock.subLocation, locationKeys[d]).drFat += +modifier : '');
-                ( (type === "imp" || typeof imp != 'undefined') ? getProperty(locationDRBlock.subLocation, locationKeys[d]).drImp += +modifier : '');
-                ( (type === "pi" || typeof pi != 'undefined') ? getProperty(locationDRBlock.subLocation, locationKeys[d]).drPi += +modifier : '');
-                ( (type === "tox" || typeof tox != 'undefined') ? getProperty(locationDRBlock.subLocation, locationKeys[d]).drTox += +modifier : '');
+                ((type === "burn" || typeof type == 'undefined') ? getProperty(locationDRBlock.subLocation, locationKeys[d]).drBurn += +modifier : '');
+                ((type === "cor" || typeof type == 'undefined') ? getProperty(locationDRBlock.subLocation, locationKeys[d]).drCor += +modifier : '');
+                ((type === "cr" || typeof type == 'undefined') ? getProperty(locationDRBlock.subLocation, locationKeys[d]).drCr += +modifier : '');
+                ((type === "cut" || typeof type == 'undefined') ? getProperty(locationDRBlock.subLocation, locationKeys[d]).drCut += +modifier : '');
+                ((type === "fat" || typeof type == 'undefined') ? getProperty(locationDRBlock.subLocation, locationKeys[d]).drFat += +modifier : '');
+                ((type === "imp" || typeof type == 'undefined') ? getProperty(locationDRBlock.subLocation, locationKeys[d]).drImp += +modifier : '');
+                ((type === "pi" || typeof type == 'undefined') ? getProperty(locationDRBlock.subLocation, locationKeys[d]).drPi += +modifier : '');
+                ((type === "tox" || typeof type == 'undefined') ? getProperty(locationDRBlock.subLocation, locationKeys[d]).drTox += +modifier : '');
             }
         }
 
         else { // The location has no sub locations
-            ( (type === "burn" || typeof burn != 'undefined') ? locationDRBlock.drBurn += +modifier : '');
-            ( (type === "cor" || typeof cor != 'undefined') ? locationDRBlock.drCor += +modifier : '');
-            ( (type === "cr" || typeof cr != 'undefined') ? locationDRBlock.drCr += +modifier : '');
-            ( (type === "cut" || typeof cut != 'undefined') ? locationDRBlock.drCut += +modifier : '');
-            ( (type === "fat" || typeof fat != 'undefined') ? locationDRBlock.drFat += +modifier : '');
-            ( (type === "imp" || typeof imp != 'undefined') ? locationDRBlock.drImp += +modifier : '');
-            ( (type === "pi" || typeof pi != 'undefined') ? locationDRBlock.drPi += +modifier : '');
-            ( (type === "tox" || typeof tox != 'undefined') ? locationDRBlock.drTox += +modifier : '');
+            ((type === "burn" || typeof type == 'undefined') ? locationDRBlock.drBurn += +modifier : '');
+            ((type === "cor" || typeof type == 'undefined') ? locationDRBlock.drCor += +modifier : '');
+            ((type === "cr" || typeof type == 'undefined') ? locationDRBlock.drCr += +modifier : '');
+            ((type === "cut" || typeof type == 'undefined') ? locationDRBlock.drCut += +modifier : '');
+            ((type === "fat" || typeof type == 'undefined') ? locationDRBlock.drFat += +modifier : '');
+            ((type === "imp" || typeof type == 'undefined') ? locationDRBlock.drImp += +modifier : '');
+            ((type === "pi" || typeof type == 'undefined') ? locationDRBlock.drPi += +modifier : '');
+            ((type === "tox" || typeof type == 'undefined') ? locationDRBlock.drTox += +modifier : '');
         }
 
         this.item.update({ [event.currentTarget.id]: locationDRBlock });
