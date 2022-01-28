eeca41d9eef02e53aae08654a7aebc2d09df26d4
Calvin <calvin.s.mcgowan@gmail.com>
2022-01-28 14:05:57 -0700

    Updating select attack macro to send a message when no token is selected

--- macros/selectMeleeAttack.js
+++ macros/selectMeleeAttack.js
@@ -1,26 +1,28 @@
-//This macro allows the user to select a melee attack from their sheet to use against their target(s)
+// This macro allows the user to select an attack from their sheet to use against their target(s)
 
-let selfToken = token;//Get owned token
-let targetSet = game.user.targets//Get set of targets
-let targetArray = Array.from(targetSet);//Convert to an actually useable data type
-let selfActor = selfToken.actor//Get owned actor from token
+let selfToken = token; // Get owned token
 
-console.log(selfToken);
-console.log(selfActor);
-console.log(targetArray);
-
-if(targetArray.length == 0){//There were no targets, show an error.
-    let noTargetsDialogContent = selfActor.noTargetsDialog();
-
-    noTargetsDialogContent.render(true);
+if (typeof selfToken == "undefined") {
+    game.gurpsAPI.noSelectionsDialog(1);
 }
+else {
+    let targetSet = game.user.targets // Get set of targets
+    let targetArray = Array.from(targetSet); // Convert to an actually useable data type
+    let selfActor = selfToken.actor // Get owned actor from token
 
-else if(targetArray.length == 1){//There is one target.
-    let singleTargetDialogContent = selfActor.singleTargetDialog(selfToken, targetArray[0]);
+    if(targetArray.length == 0){//There were no targets, show an error.
+        let noTargetsDialogContent = selfActor.noTargetsDialog();
 
-    singleTargetDialogContent.render(true);
-}
+        noTargetsDialogContent.render(true);
+    }
 
-else if(targetArray.length > 1){//There is more than one target.
+    else if(targetArray.length == 1){//There is one target.
+        let singleTargetDialogContent = selfActor.singleTargetDialog(selfToken, targetArray[0]);
 
+        singleTargetDialogContent.render(true);
+    }
+
+    else if(targetArray.length > 1){//There is more than one target.
+
+    }
 }
