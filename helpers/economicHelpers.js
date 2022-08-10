5cc43a64808cdd9609cca550823ef67f3873a648
Calvin <calvin.s.mcgowan@gmail.com>
2022-08-10 15:51:02 -0600

    Added some more economic helpers surrounding starting cash and monthly pay

--- helpers/economicHelpers.js
+++ helpers/economicHelpers.js
@@ -43,4 +43,104 @@ export class economicHelpers {
 
         return col;
     }
+
+    static getStartingCashByTL(tl) {
+        let cash = 0;
+        switch(tl) {
+            case 0:
+                cash = 250;
+                break;
+            case 1:
+                cash = 500;
+                break;
+            case 2:
+                cash = 750;
+                break;
+            case 3:
+                cash = 1000;
+                break;
+            case 4:
+                cash = 2000;
+                break;
+            case 5:
+                cash = 5000;
+                break;
+            case 6:
+                cash = 10000;
+                break;
+            case 7:
+                cash = 15000;
+                break;
+            case 8:
+                cash = 20000;
+                break;
+            case 9:
+                cash = 30000;
+                break;
+            case 10:
+                cash = 50000;
+                break;
+            case 11:
+                cash = 75000;
+                break;
+            case 12:
+                cash = 100000;
+                break;
+            default:
+                cash = 1;
+                break;
+        }
+
+        return cash;
+    }
+
+    static getMonthlyPayByTL(tl) {
+        let cash = 0;
+        switch(tl) {
+            case 0:
+                cash = 625;
+                break;
+            case 1:
+                cash = 650;
+                break;
+            case 2:
+                cash = 675;
+                break;
+            case 3:
+                cash = 700;
+                break;
+            case 4:
+                cash = 800;
+                break;
+            case 5:
+                cash = 1100;
+                break;
+            case 6:
+                cash = 1600;
+                break;
+            case 7:
+                cash = 2100;
+                break;
+            case 8:
+                cash = 2600;
+                break;
+            case 9:
+                cash = 3600;
+                break;
+            case 10:
+                cash = 5600;
+                break;
+            case 11:
+                cash = 8100;
+                break;
+            case 12:
+                cash = 10600;
+                break;
+            default:
+                cash = 1;
+                break;
+        }
+
+        return cash;
+    }
 }
