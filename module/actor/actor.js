4d4933de8166fadea6b1db56f89f0af7eeee3f8d
Calvin <calvin.s.mcgowan@gmail.com>
2023-09-19 13:23:53 -0600

    Refactoring gatherOptions method name to be more descriptive

--- module/actor/actor.js
+++ module/actor/actor.js
@@ -3938,7 +3938,7 @@ export class gurpsActor extends Actor {
 				icon: '<svg aria-hidden="true" focusable="false" data-prefix="fas" role="img" viewBox="0 0 512 512" style="width: 17px;"><path fill="currentColor" d="M272 96c26.51 0 48-21.49 48-48S298.51 0 272 0s-48 21.49-48 48 21.49 48 48 48zM113.69 317.47l-14.8 34.52H32c-17.67 0-32 14.33-32 32s14.33 32 32 32h77.45c19.25 0 36.58-11.44 44.11-29.09l8.79-20.52-10.67-6.3c-17.32-10.23-30.06-25.37-37.99-42.61zM384 223.99h-44.03l-26.06-53.25c-12.5-25.55-35.45-44.23-61.78-50.94l-71.08-21.14c-28.3-6.8-57.77-.55-80.84 17.14l-39.67 30.41c-14.03 10.75-16.69 30.83-5.92 44.86s30.84 16.66 44.86 5.92l39.69-30.41c7.67-5.89 17.44-8 25.27-6.14l14.7 4.37-37.46 87.39c-12.62 29.48-1.31 64.01 26.3 80.31l84.98 50.17-27.47 87.73c-5.28 16.86 4.11 34.81 20.97 40.09 3.19 1 6.41 1.48 9.58 1.48 13.61 0 26.23-8.77 30.52-22.45l31.64-101.06c5.91-20.77-2.89-43.08-21.64-54.39l-61.24-36.14 31.31-78.28 20.27 41.43c8 16.34 24.92 26.89 43.11 26.89H384c17.67 0 32-14.33 32-32s-14.33-31.99-32-31.99z" class=""></path></svg>',
 				label: "Dodge",
 				callback: (html) => {
-					this.gatherOptions(html, "dodge", flags, locationIDs, facing[0])
+					this.gatherActiveDefenceAndOptions(html, "dodge", flags, locationIDs, facing[0])
 				}
 			}
 			width += 200; // Add width for this column of defences
@@ -3948,7 +3948,7 @@ export class gurpsActor extends Actor {
 				icon: '<svg aria-hidden="true" focusable="false" data-prefix="fas" role="img" viewBox="0 0 512 512" style="width: 14px;"><path fill="currentColor" d="M466.5 83.7l-192-80a48.15 48.15 0 0 0-36.9 0l-192 80C27.7 91.1 16 108.6 16 128c0 198.5 114.5 335.7 221.5 380.3 11.8 4.9 25.1 4.9 36.9 0C360.1 472.6 496 349.3 496 128c0-19.4-11.7-36.9-29.5-44.3zM256.1 446.3l-.1-381 175.9 73.3c-3.3 151.4-82.1 261.1-175.8 307.7z" class=""></path></svg>',
 				label: "Block",
 				callback: (html) => {
-					this.gatherOptions(html, "block", flags, locationIDs, facing[0])
+					this.gatherActiveDefenceAndOptions(html, "block", flags, locationIDs, facing[0])
 				}
 			}
 			width += 200; // Add width for this column of defences
@@ -3958,7 +3958,7 @@ export class gurpsActor extends Actor {
 				icon: '<svg aria-hidden="true" focusable="false" data-prefix="fas" role="img" viewBox="0 0 512 512" style="width: 14px;"><path fill="currentColor" d="M507.31 462.06L448 402.75l31.64-59.03c3.33-6.22 2.2-13.88-2.79-18.87l-17.54-17.53c-6.25-6.25-16.38-6.25-22.63 0L420 324 112 16 18.27.16C8.27-1.27-1.42 7.17.17 18.26l15.84 93.73 308 308-16.69 16.69c-6.25 6.25-6.25 16.38 0 22.62l17.53 17.54a16 16 0 0 0 18.87 2.79L402.75 448l59.31 59.31c6.25 6.25 16.38 6.25 22.63 0l22.62-22.62c6.25-6.25 6.25-16.38 0-22.63zm-149.36-76.01L60.78 88.89l-5.72-33.83 33.84 5.72 297.17 297.16-28.12 28.11zm65.17-325.27l33.83-5.72-5.72 33.84L340.7 199.43l33.94 33.94L496.01 112l15.84-93.73c1.43-10-7.01-19.69-18.1-18.1l-93.73 15.84-121.38 121.36 33.94 33.94L423.12 60.78zM199.45 340.69l-45.38 45.38-28.12-28.12 45.38-45.38-33.94-33.94-45.38 45.38-16.69-16.69c-6.25-6.25-16.38-6.25-22.62 0l-17.54 17.53a16 16 0 0 0-2.79 18.87L64 402.75 4.69 462.06c-6.25 6.25-6.25 16.38 0 22.63l22.62 22.62c6.25 6.25 16.38 6.25 22.63 0L109.25 448l59.03 31.64c6.22 3.33 13.88 2.2 18.87-2.79l17.53-17.54c6.25-6.25 6.25-16.38 0-22.63L188 420l45.38-45.38-33.93-33.93z" class=""></path></svg>',
 				label: "Parry",
 				callback: (html) => {
-					this.gatherOptions(html, "parry", flags, locationIDs, facing[0])
+					this.gatherActiveDefenceAndOptions(html, "parry", flags, locationIDs, facing[0])
 				}
 			}
 			width += 200; // Add width for this column of defences
@@ -3968,7 +3968,7 @@ export class gurpsActor extends Actor {
 				icon: '<svg aria-hidden="true" focusable="false" data-prefix="fas" role="img" viewBox="0 0 512 512" style="width: 14px;"><path fill="currentColor" d="M216 23.86c0-23.8-30.65-32.77-44.15-13.04C48 191.85 224 200 224 288c0 35.63-29.11 64.46-64.85 63.99-35.17-.45-63.15-29.77-63.15-64.94v-85.51c0-21.7-26.47-32.23-41.43-16.5C27.8 213.16 0 261.33 0 320c0 105.87 86.13 192 192 192s192-86.13 192-192c0-170.29-168-193-168-296.14z" class=""></path></svg>',
 				label: "Magical",
 				callback: (html) => {
-					this.gatherOptions(html, "magical", flags, locationIDs, facing[0])
+					this.gatherActiveDefenceAndOptions(html, "magical", flags, locationIDs, facing[0])
 				}
 			}
 			width += 200; // Add width for this column of defences
@@ -4009,7 +4009,8 @@ export class gurpsActor extends Actor {
 		this.applyDamage(flags, locationIDs);
 	}
 
-	gatherOptions(html, type, flags, locationIDs, facing){
+	// This method gathers the selections the user has made when selecting their active defence and any relevant modifiers
+	gatherActiveDefenceAndOptions(html, type, flags, locationIDs, facing){
 		let selection;
 		let name;
 		let mod = parseInt(html.find('#mod').val());
