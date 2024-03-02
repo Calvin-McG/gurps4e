e922e76fde6d64f8b6e3a001fb4fcdeef5d2299d
Calvin <calvin.s.mcgowan@gmail.com>
2024-03-01 18:58:05 -0700

    Corrected equip check to only check on equipment

--- module/actor/actor.js
+++ module/actor/actor.js
@@ -2682,7 +2682,7 @@ export class gurpsActor extends Actor {
 		let affliction;
 
 		actor.items.forEach((item) => {
-			if ((item.type == "Trait" || item.type == "Equipment" || item.type == "Spell" || item.type == "Custom Weapon" || item.type == "Custom Armour" || item.type == "Travel Fare" || (item.type == "Ritual" && item.system.quantity > 0)) && item.system.equipStatus === "equipped"){
+			if ((item.type == "Trait" || (item.type == "Equipment" && item.system.equipStatus === "equipped") || item.type == "Spell" || item.type == "Custom Weapon" || item.type == "Custom Armour" || item.type == "Travel Fare" || (item.type == "Ritual" && item.system.quantity > 0))){
 				if (item.system.melee) {
 					let meleeKeys = Object.keys(item.system.melee); // Collect all the melee keys
 					for (let m = 0; m < meleeKeys.length; m++){
