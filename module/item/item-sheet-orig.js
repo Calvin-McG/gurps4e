a76c5ee599674ff4b285fa81018199c4d1be64c4
Calvin <calvin.s.mcgowan@gmail.com>
2024-05-26 15:37:54 -0600

    Updated logic around dropdown objects so that custom weapons only load dropdowns relevant to their current weapon type

--- module/item/item-sheet-orig.js
+++ module/item/item-sheet-orig.js
@@ -42,30 +42,36 @@ export class gurpsItemSheet extends ItemSheet {
 
         if (this.item.type === "Custom Weapon") {
             context.customWeaponTypes = this.getCustomWeaponTypes();
-            context.bowShapes = CONFIG.BOWSHAPES.dropdownChoices;
-            context.arrowDamageTypes = CONFIG.ARROWDAMAGETYPES.dropdownChoices;
-            context.arrowArmourDivisors = CONFIG.ARROWARMOURDIVISOR.dropdownChoices;
-            context.bowConstructionTypes = this.getBowConstructionTypes();
-            context.firearmConfigurations = this.getFirearmConfigurations();
-            context.firearmActionTypes = this.getFirearmActionTypes();
-            context.firearmLockTypes = this.getFirearmLockTypes();
-            context.firearmBoltTypes = this.getFirearmBoltTypes();
-            context.firearmPropellants = this.getFirearmPropellants();
-            context.cartridgeTypes = this.getCartridgeTypes();
-            context.magazineStyles = this.getMagazineStyles();
-            context.magazineMaterials = this.getMagazineMaterials();
-            context.firearmQualityAccuracy = this.getFirearmQualityAccuracy();
-            context.firearmQualityReliability = this.getFirearmQualityReliability();
-            context.firearmCasingType = this.getFirearmCasingTypes();
-            context.ammunitionGrades = this.getAmmunitionGrades();
-            context.projectileTypes = this.getProjectileTypes();
-            context.laserConfigurations = this.getLaserConfiguration();
-            context.laserBeamTypes = this.getLaserBeamTypes();
-            context.laserColours = this.getLaserColours();
-            context.laserGenerators = this.getLaserGenerators();
-            context.graviticFocusing = this.getGraviticFocusing();
-            context.powerCells = CONFIG.POWERCELLS.dropdownChoices;
-            context.explosiveFillers = this.getExplosiveFillers();
+            if (this.item.system.customType === "laser") {
+                context.laserConfigurations = this.getLaserConfiguration();
+                context.laserBeamTypes = this.getLaserBeamTypes();
+                context.laserColours = this.getLaserColours();
+                context.laserGenerators = this.getLaserGenerators();
+                context.graviticFocusing = this.getGraviticFocusing();
+                context.powerCells = CONFIG.POWERCELLS.dropdownChoices;
+            }
+            if (this.item.system.customType === "firearm") {
+                context.firearmConfigurations = this.getFirearmConfigurations();
+                context.firearmActionTypes = this.getFirearmActionTypes();
+                context.firearmLockTypes = this.getFirearmLockTypes();
+                context.firearmBoltTypes = this.getFirearmBoltTypes();
+                context.firearmPropellants = this.getFirearmPropellants();
+                context.cartridgeTypes = this.getCartridgeTypes();
+                context.magazineStyles = this.getMagazineStyles();
+                context.magazineMaterials = this.getMagazineMaterials();
+                context.firearmQualityAccuracy = this.getFirearmQualityAccuracy();
+                context.firearmQualityReliability = this.getFirearmQualityReliability();
+                context.firearmCasingType = this.getFirearmCasingTypes();
+                context.ammunitionGrades = this.getAmmunitionGrades();
+                context.projectileTypes = this.getProjectileTypes();
+                context.explosiveFillers = this.getExplosiveFillers();
+            }
+            if (this.item.system.customType === "bow" || this.item.system.customType === "xbow" || this.item.system.customType === "footbow") {
+                context.bowShapes = CONFIG.BOWSHAPES.dropdownChoices;
+                context.arrowDamageTypes = CONFIG.ARROWDAMAGETYPES.dropdownChoices;
+                context.arrowArmourDivisors = CONFIG.ARROWARMOURDIVISOR.dropdownChoices;
+                context.bowConstructionTypes = this.getBowConstructionTypes();
+            }
         }
 
         if (this.item.type === "Custom Armour") {
